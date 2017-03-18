07e777fb92203e7313db07cc5e96d2f099d0de6b
womo <wangjifeng1996@gmail.com>
2017-03-18 15:22:19 +0800

    remove module

--- app/src/main/java/com/example/cieo233/notetest/NoteListActivity.java
+++ app/src/main/java/com/example/cieo233/notetest/NoteListActivity.java
@@ -23,7 +23,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.example.administrator.noteediter.NoteRichEditor;
 import com.transitionseverywhere.AutoTransition;
 import com.transitionseverywhere.TransitionManager;
 
